fe5e879512e423387e952268b300db1132855900
Otxoa Lardeux <olardeux@made-f0Cr2s1.clusters.42paris.fr>
2025-01-07 11:11:15 +0100

    init

---
+++ inc/cube3D.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cube3D.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olardeux <olardeux@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/07 10:37:18 by olardeux          #+#    #+#             */
+/*   Updated: 2025/01/07 11:04:47 by olardeux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUBE3D_H
+# define CUBE3D_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <mlx.h>
+#include <time.h>
+#include <sys/time.h>
+
+#endif
