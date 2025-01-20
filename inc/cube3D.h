80ce10a688d9fd99491e919b5a82384e250e7e1e
Otxoa Lardeux <olardeux@bess-f2r2s13.clusters.42paris.fr>
2025-01-20 11:36:57 +0100

    init hook

--- inc/cube3D.h
+++ inc/cube3D.h
@@ -6,21 +6,37 @@
 /*   By: olardeux <olardeux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 10:37:18 by olardeux          #+#    #+#             */
-/*   Updated: 2025/01/07 11:04:47 by olardeux         ###   ########.fr       */
+/*   Updated: 2025/01/20 10:16:39 by olardeux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUBE3D_H
 # define CUBE3D_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <mlx.h>
-#include <time.h>
-#include <sys/time.h>
+# include <fcntl.h>
+# include <math.h>
+# include <mlx.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <time.h>
+# include <unistd.h>
+
+typedef struct s_img
+{
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}			t_img;
+
+typedef struct s_data
+{
+	void	*mlx;
+	void	*win;
+	t_img	img;
+}			t_data;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,14 +6,48 @@
 /*   By: olardeux <olardeux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 10:36:46 by olardeux          #+#    #+#             */
-/*   Updated: 2025/01/07 11:09:34 by olardeux         ###   ########.fr       */
+/*   Updated: 2025/01/20 11:34:41 by olardeux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3D.h"
 
+int	destroy(t_data *data)
+{
+	mlx_destroy_window(data->mlx, data->win);
+	mlx_destroy_image(data->mlx, data->img.img);
+	mlx_destroy_display(data->mlx);
+	free(data->mlx);
+	exit(0);
+}
+
+int	key_hook(int keycode, t_data *data)
+{
+	printf("keycode: %d\n", keycode);
+	if (keycode == 65307)
+		destroy(data);
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
-	(void)argc;
+	t_data data;
+
 	(void)argv;
+	if (argc == 2)
+	{
+		data.mlx = mlx_init();
+		data.win = mlx_new_window(data.mlx, 1920, 1080, "cube3D");
+		data.img.img = mlx_new_image(data.mlx, 1920, 1080);
+		data.img.addr = mlx_get_data_addr(data.img.img, &data.img.bpp,
+				&data.img.line_len, &data.img.endian);
+		mlx_hook(data.win, 17, (1L << 17), destroy, &data);
+		mlx_hook(data.win, 2, (1L << 0), key_hook, &data);
+		mlx_loop(data.mlx);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
 }
