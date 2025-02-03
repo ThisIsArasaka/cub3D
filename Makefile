eb37b0cc2c813130281976bade44defe4c7c41e6
Otxoa Lardeux <olardeux@bess-f2r1s15.clusters.42paris.fr>
2025-02-03 11:33:19 +0100

    return

---
+++ Makefile
@@ -0,0 +1,51 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: olardeux <olardeux@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/01/07 10:10:04 by olardeux          #+#    #+#              #
+#    Updated: 2025/01/28 12:35:57 by olardeux         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = cub3D
+CC = clang
+CFLAGS = -Wall -Wextra -Werror -g3
+MLXFLAGS = -lXext -lX11 -lm -lz
+MLX_DIR = ./minilibx-linux
+MLX = $(MLX_DIR)/libmlx.a
+SRC_DIR = ./src
+OBJ_DIR = ./obj
+INC_DIR = ./inc
+SRC_FILE = main.c draw.c move.c init.c
+SRC = $(addprefix $(SRC_DIR)/, $(SRC_FILE))
+OBJ = $(addprefix $(OBJ_DIR)/, $(SRC_FILE:.c=.o))
+
+all: $(NAME)
+
+$(NAME): $(MLX) $(OBJ)
+	@$(CC) $(CFLAGS) -I $(INC_DIR) -I $(MLX_DIR) $(OBJ) $(MLX) $(MLXFLAGS) -o $(NAME)
+	@echo "\033[32m$(NAME) is compiled\033[0m"
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	@mkdir -p $(OBJ_DIR)
+	@$(CC) $(CFLAGS) -I $(INC_DIR) -I $(MLX_DIR) -c $< -o $@
+	@echo "\033[33m$@ is compiled\033[0m"
+
+$(MLX):
+	@make -C $(MLX_DIR)
+
+clean:
+	@rm -rf $(OBJ_DIR)
+	@echo "\033[31mObject files are removed\033[0m"
+
+fclean: clean
+	@rm -f $(NAME)
+	@echo "\033[31m$(NAME) is removed\033[0m"
+
+re: fclean all
+	@echo "\033[32m$(NAME) is recompiled\033[0m"
+
+.PHONY: all clean fclean re
---
+++ src/draw.c
@@ -0,0 +1,313 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olardeux <olardeux@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/21 09:02:02 by olardeux          #+#    #+#             */
+/*   Updated: 2025/02/03 11:22:58 by olardeux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cube3D.h"
+
+void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
+{
+	char	*dst;
+
+	if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
+		return ;
+	dst = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	*(unsigned int *)dst = color;
+}
+
+int	get_pixel_color(t_img *img, int x, int y)
+{
+	char	*dst;
+
+	if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
+		return (0);
+	dst = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	return (*(unsigned int *)dst);
+}
+
+void	draw_floor(t_data *data)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	while (x < WIDTH)
+	{
+		y = HEIGHT / 2;
+		while (y < HEIGHT)
+		{
+			my_mlx_pixel_put(&data->img, x, y, data->map.floor_color);
+			y++;
+		}
+		x++;
+	}
+}
+
+void	draw_ceiling(t_data *data)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	while (x < WIDTH)
+	{
+		y = 0;
+		while (y < HEIGHT / 2)
+		{
+			my_mlx_pixel_put(&data->img, x, y, data->map.ceiling_color);
+			y++;
+		}
+		x++;
+	}
+}
+
+void	put_wall_pixel(t_data *data, int x, int side)
+{
+	int		wall_height;
+	int		wall_start;
+	int		wall_end;
+	int		color;
+	double	wall_x;
+	int		texture_x;
+	int		texture_y;
+	double	step;
+	double	tex_pos;
+	t_img	*texture;
+
+	wall_height = HEIGHT / data->ray.wall_dist;
+	wall_start = HEIGHT / 2 - wall_height / 2;
+	wall_end = HEIGHT / 2 + wall_height / 2;
+	if (side == 0)
+	{
+		if (data->ray.ray_dirx > 0)
+			texture = &data->texture.east;
+		else
+			texture = &data->texture.west;
+	}
+	else
+	{
+		if (data->ray.ray_diry > 0)
+			texture = &data->texture.south;
+		else
+			texture = &data->texture.north;
+	}
+	if (side == 0)
+		wall_x = data->player.y + data->ray.wall_dist * data->ray.ray_diry;
+	else
+		wall_x = data->player.x + data->ray.wall_dist * data->ray.ray_dirx;
+	wall_x -= floor(wall_x);
+	texture_x = wall_x * texture->width;
+	if (side == 0 && data->ray.ray_dirx > 0)
+		texture_x = texture->width - texture_x - 1;
+	if (side == 1 && data->ray.ray_diry < 0)
+		texture_x = texture->width - texture_x - 1;
+	step = 1.0 * texture->height / wall_height;
+	tex_pos = (wall_start - HEIGHT / 2 + wall_height / 2) * step;
+	while (wall_start < wall_end)
+	{
+		texture_y = (int)tex_pos % (texture->height - 1);
+		tex_pos += step;
+		color = get_pixel_color(texture, texture_x, texture_y);
+		my_mlx_pixel_put(&data->img, x, wall_start, color);
+		wall_start++;
+	}
+}
+
+void	dda(t_data *data, int x)
+{
+	int		hit;
+	int		side;
+	int		map_x;
+	int		map_y;
+	double	delta_distx;
+	double	delta_disty;
+	int		map[15][15] = {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0}, {1, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1}, {1, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 1, 0, 0, 1}, {1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1,
+				1}, {1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1}, {1, 0, 1, 0,
+				0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1}, {1, 0, 1, 1, 1, 1, 1, 1, 0, 0,
+				0, 0, 1, 0, 1}, {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+				{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, {1, 0, 1, 0, 0,
+				0, 0, 1, 0, 0, 0, 0, 1, 0, 1}, {1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0,
+				0, 1, 0, 1}, {1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1}, {1,
+				0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1}, {1, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 1}, {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+				1, 1}};
+
+	// int		map[9][10] = {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, {1, 0, 0, 0, 0, 0,
+	//			0,
+	// 			0, 0, 1}, {1, 0, 0, 0, 0, 0, 0, 0, 0, 1}, {1, 0, 1, 0, 0, 0, 0,
+	// 			1, 0, 1}, {1, 0, 1, 0, 0, 0, 0, 1, 0, 1}, {1, 0, 1, 0, 0, 0, 0,
+	// 			1, 0, 1}, {1, 0, 1, 1, 1, 1, 1, 1, 0, 1}, {1, 0, 0, 0, 0, 0, 0,
+	// 			0, 0, 1}, {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+	hit = 0;
+	map_x = (int)data->player.x;
+	map_y = (int)data->player.y;
+	if (data->ray.ray_dirx == 0)
+		delta_distx = 1e30;
+	else
+		delta_distx = fabs(1 / data->ray.ray_dirx);
+	if (data->ray.ray_diry == 0)
+		delta_disty = 1e30;
+	else
+		delta_disty = fabs(1 / data->ray.ray_diry);
+	if (data->ray.ray_dirx < 0)
+	{
+		data->ray.stepx = -1;
+		data->ray.distx = (data->player.x - (int)data->player.x) * delta_distx;
+	}
+	else
+	{
+		data->ray.stepx = 1;
+		data->ray.distx = ((int)data->player.x + 1 - data->player.x)
+			* delta_distx;
+	}
+	if (data->ray.ray_diry < 0)
+	{
+		data->ray.stepy = -1;
+		data->ray.disty = (data->player.y - (int)data->player.y) * delta_disty;
+	}
+	else
+	{
+		data->ray.stepy = 1;
+		data->ray.disty = ((int)data->player.y + 1 - data->player.y)
+			* delta_disty;
+	}
+	while (!hit)
+	{
+		if (data->ray.distx < data->ray.disty)
+		{
+			data->ray.distx += delta_distx;
+			map_x += data->ray.stepx;
+			side = 0;
+		}
+		else
+		{
+			data->ray.disty += delta_disty;
+			map_y += data->ray.stepy;
+			side = 1;
+		}
+		if (map[map_y][map_x] == 1)
+			hit = 1;
+	}
+	if (side == 0)
+	{
+		data->ray.wall_dist = (map_x - data->player.x + (1 - data->ray.stepx)
+				/ 2) / data->ray.ray_dirx;
+	}
+	else
+	{
+		data->ray.wall_dist = (map_y - data->player.y + (1 - data->ray.stepy)
+				/ 2) / data->ray.ray_diry;
+	}
+	if (data->ray.wall_dist < 0)
+		data->ray.wall_dist = 1e30;
+	put_wall_pixel(data, x, side);
+}
+
+void	draw_walls(t_data *data)
+{
+	int	x;
+
+	x = 0;
+	while (x < WIDTH)
+	{
+		data->ray.ray_angle = data->player.dir - FOV / 2 + (x * FOV / WIDTH);
+		data->ray.ray_dirx = cos(data->ray.ray_angle);
+		data->ray.ray_diry = sin(data->ray.ray_angle);
+		dda(data, x);
+		x++;
+	}
+}
+
+void	draw_player_map(t_data *data)
+{
+	int		player_x;
+	int		player_y;
+	int		arrow_x;
+	int		arrow_y;
+	double	angle;
+	int		i;
+
+	player_x = 75 + cos(data->player.dir) * 5;
+	player_y = 75 + sin(data->player.dir) * 5;
+	i = 1;
+	while (i < 10)
+	{
+		angle = data->player.dir + (PI * 5) / 6;
+		arrow_x = player_x + cos(angle) * i;
+		arrow_y = player_y + sin(angle) * i;
+		my_mlx_pixel_put(&data->img, arrow_x, arrow_y, 0x00FF0000);
+		angle = data->player.dir - (PI * 5) / 6;
+		arrow_x = player_x + cos(angle) * i;
+		arrow_y = player_y + sin(angle) * i;
+		my_mlx_pixel_put(&data->img, arrow_x, arrow_y, 0x00FF0000);
+		i++;
+	}
+	player_x = arrow_x;
+	player_y = arrow_y;
+	i = 1;
+	while (i < 10)
+	{
+		angle = data->player.dir + PI / 2;
+		arrow_x = player_x + cos(angle) * i;
+		arrow_y = player_y + sin(angle) * i;
+		my_mlx_pixel_put(&data->img, arrow_x, arrow_y, 0x00FF0000);
+		i++;
+	}
+}
+
+void	draw_map(t_data *data)
+{
+	int		map[15][15] = {
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0}, 
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1}, 
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1}, 
+	{1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1}, 
+	{1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1}, 
+	{1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1}, 
+	{1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1}, 
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, 
+	{1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1}, 
+	{1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1}, 
+	{1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1}, 
+	{1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1}, 
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, 
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+	int		screen_x;
+	int		screen_y;
+	double	map_x;
+	double	map_y;
+
+	screen_y = 0;
+	while (screen_y < 150)
+	{
+		screen_x = 0;
+		while (screen_x < 150)
+		{
+			map_x = data->player.x - 5 + ((double)screen_x - 25) / MINIMAP_SIZE;
+			map_y = data->player.y - 5 + ((double)screen_y - 25) / MINIMAP_SIZE;
+			if (map_x >= 0 && map_x < data->map.width && map_y >= 0
+				&& map_y < data->map.height)
+			{
+				if (map[(int)map_y][(int)map_x] == 1)
+					my_mlx_pixel_put(&data->img, screen_x, screen_y,
+						0x00FF0000);
+				else
+					my_mlx_pixel_put(&data->img, screen_x, screen_y,
+						0x6600FF00);
+			}
+			screen_x++;
+		}
+		screen_y++;
+	}
+	draw_player_map(data);
+}
